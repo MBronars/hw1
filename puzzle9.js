b33827c3415e87da8aa3bb8c54c00949223267f0
MBronars <35738375+MBronars@users.noreply.github.com>
2018-01-23 13:44:41 -0800

    Create puzzle9.js

---
+++ puzzle9.js
@@ -0,0 +1,21 @@
+count = 4;
+while (count > 0){
+  setColor('blue');
+  down();
+  count = count - 1;
+}
+count = 2;
+while(count > 0){
+  right();
+  setColor('blue');
+  count = count - 1;
+}
+count = 3;
+while(count> 0){
+  up();
+  setColor('blue');
+  count = count - 1;
+  }
+left();
+setColor('blue');
+  
