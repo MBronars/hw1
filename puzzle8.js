79281b5dac587947cecd5c17e529bfabaa1dc3e0
MBronars <35738375+MBronars@users.noreply.github.com>
2018-01-23 13:32:03 -0800

    Create puzzle8.js

---
+++ puzzle8.js
@@ -0,0 +1,25 @@
+steps = 8;
+while(steps > 0)
+  down();
+  steps = steps-1;
+right();
+right();
+while(steps < 7)
+  up();
+  steps = steps+1;
+right();
+right();
+while(steps > 0)
+  down();
+  steps = steps-1;
+right();
+right();
+while(steps < 7)
+  up();
+  steps = steps+1;
+right();
+right();
+while(steps > 0)
+  down();
+  steps = steps-1;
+right();
