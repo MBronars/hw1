4fa8a88fe48e3e9974dd17563c9838402469018b
MBronars <35738375+MBronars@users.noreply.github.com>
2018-01-23 14:00:33 -0800

    Update puzzle8.js

--- puzzle8.js
+++ puzzle8.js
@@ -1,25 +1,30 @@
 steps = 8;
-while(steps > 0)
+while(steps > 0){
   down();
   steps = steps-1;
+}
 right();
 right();
-while(steps < 7)
+while(steps < 7){
   up();
   steps = steps+1;
+}
 right();
 right();
-while(steps > 0)
+while(steps > 0){
   down();
   steps = steps-1;
+}
 right();
 right();
-while(steps < 7)
+while(steps < 7){
   up();
   steps = steps+1;
+}
 right();
 right();
-while(steps > 0)
+while(steps > 0){
   down();
   steps = steps-1;
+}
 right();
